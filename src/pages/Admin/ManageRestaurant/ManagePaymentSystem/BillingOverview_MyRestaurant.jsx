e96141b6a30a7fb359d7d3a18c52361f2332d6b3
Hossain Ahamed <hossainahamed6872@gmail.com>
2024-01-28 11:56:54 +0600

    pointer

--- src/pages/Admin/ManageRestaurant/ManagePaymentSystem/BillingOverview_MyRestaurant.jsx
+++ src/pages/Admin/ManageRestaurant/ManagePaymentSystem/BillingOverview_MyRestaurant.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import SetTitle from '../../../Shared/SetTtitle/SetTitle';
-
+import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
 const BillingOverview_MyRestaurant = () => {
     return (
         <section>
-           <SetTitle title="Billing Overview"/> 
+            <SetTitle title="Billing Overview" />
+            <Tabs  aria-label="Disabled Options" className='w-full justify-center'>
+                <Tab key="billingHistory" title="Billing History">
+                    kdjasfjdsjaflskjsdalkf
+                </Tab>
+                <Tab key="Subscriptions" title="Subscriptions">
+                    dasflkkdasjkfjldksafjlfdjsl
+                </Tab>
+            </Tabs>
         </section>
     );
 };
