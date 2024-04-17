7c8c5c563a3f076640ef30add24160e52b4c022b
Hossain Ahamed <hossainahamed6872@gmail.com>
2024-04-18 05:58:21 +1000

    382

--- src/pages/Admin/Inventory/RecipeList/AddRecipe.jsx
+++ src/pages/Admin/Inventory/RecipeList/AddRecipe.jsx
@@ -29,8 +29,9 @@ const AddRecipe = () => {
     });
     const { branchID, res_id } = useRestauarantAndBranch();
     const units = getUnits();
-    const { refetch, data: data = [], isLoading, error } = useQuery({
+    const { refetch, data, isLoading, error } = useQuery({
         queryKey: ['dish-receipe-data-kasjfoiu438u', branchID, res_id,dishID],
+        cacheTime:0,
         queryFn: async () => {
             const res = await axiosSecure.get(`/admin/restaurant/${res_id}/branch/${branchID}/get-dishes/${dishID}`);
 
@@ -44,7 +45,7 @@ const AddRecipe = () => {
             //     ]
             // }
             // setValue("ingredients", data.ingredients)
-            setValue("dish", data.title)
+            // setValue("dish", data.title)
             return res.data
             // return res.data
         }
@@ -82,10 +83,10 @@ const AddRecipe = () => {
                         <div className="flex flex-wrap pb-3 m-3 border-1 rounded">
                             <div className="w-full  p-3">
                                 <p className="mb-1.5 font-medium text-base text-gray-800" data-config-id="auto-txt-3-3">Title</p>
-                                <input className="disabled:cursor-not-allowed w-full px-4 py-2.5 text-base text-gray-900 font-normal outline-none focus:border-green-500 border border-gray-300 rounded-lg shadow-input" type="text" placeholder="ie: Rice Bowl"
+                                <input defaultValue={data?.title} className="disabled:cursor-not-allowed w-full px-4 py-2.5 text-base text-gray-900 font-normal outline-none focus:border-green-500 border border-gray-300 rounded-lg shadow-input" type="text" placeholder="ie: Rice Bowl"
                                     {...register("dish", {
                                         required: "*Dish  is Required",
-                                    })} disabled />
+                                    })} readOnly />
                                 {errors.dish?.type === "required" && (<p className='m-0 p-0 pl-1  text-base text-red-500 text-[9px]' role="alert">{errors?.dish?.message}</p>)}
 
                             </div>
